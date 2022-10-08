aecac989b7e005d35caf7170fec21eaf1960b230
minh_la_Ngoc_day <minhngocyn1@gmail.com>
2022-10-08 20:23:21 +0700

    kfjas;dkflasj

--- lib/all_constants.dart
+++ lib/all_constants.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 
 const mainColor = Colors.pinkAccent;
 
-const cardTextStyle = TextStyle(fontSize: 20, letterSpacing: 1.0);
+const cardTextStyle = TextStyle(
+  fontSize: 20,
+  letterSpacing: 1.0,
+);
 const otherTextStyle = TextStyle(fontSize: 15);
